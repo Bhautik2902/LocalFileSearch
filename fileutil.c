8f3ea1eb9c5b037fd00cf107c5c1240c8300bedc
gOgRABgEEK <bhautiksavaliya2000@gmail.com>
2024-01-31 20:09:31 -0500

    added code for arguments and validated path

--- fileutil.c
+++ fileutil.c
@@ -1,6 +1,47 @@
 #include<stdio.h>
 #include<ftw.h>
+#include<stdbool.h>
+#include<regex.h>
 
-void main() {
+// fileutil [ root_dir] filename 
+// fileutil [root_dir] [storage_dir] [options] filename
+// fileutil [root_dir] [storage_dir] extension
+   
+void main(int argc, char *argv[]) {
 
+    // validating the command.
+    if (argc == 3) {
+
+    }
+    else if (argc == 4) {
+        
+    }
+    else if (argc == 5) {
+        /* code */
+    }
+    else if (argc < 3) {
+        printf("fileutil: Not enough arguments\n");
+    }
+    else {
+        printf("fileutil: Too many arguments\n");
+    }
+    
 }
+
+bool validatePath (char *path) {
+    regex_t rx;
+    int retFlag;
+
+    const char *ptn = "^(\\.?\\.?/|/)?([-_a-zA-Z0-9]+/)*[-_a-zA-Z0-9]+$";
+
+    // Compiling the pattern.
+    retFlag = regcomp(&rx, ptn, REG_EXTENDED);
+    if (retFlag != 0) {
+        printf("Failed to compile regex\n");
+        return 0;  
+    }
+
+    // matching the pattern with file.
+    retFlag = regexec(&rx, path, 0, NULL, 0);
+}
+
