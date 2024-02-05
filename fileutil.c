f96251a7435b05df3c4aca855ccec84781934ade
gOgRABgEEK <bhautiksavaliya2000@gmail.com>
2024-02-05 06:12:26 -0500

    Added implementation for creating tar of matching file extention

--- fileutil.c
+++ fileutil.c
@@ -2,6 +2,7 @@
 #include<ftw.h>
 #include<regex.h>
 #include<string.h>
+#include<stdlib.h>
 
 // fileutil [ root_dir] filename 
 // fileutil [root_dir] [storage_dir] [options] filename
@@ -10,6 +11,8 @@
 char *targetFile;
 char *opr;  // copy or move option.
 char *destinationFolder;  // to copy or move into.
+int fileCount=0;
+char *exten;  // extention of the file provided by user.
 
 struct FTW {
     int base;
@@ -39,6 +42,14 @@ int validatePath (char *path) {
     }
 
     regfree(&rx);   
+}
+
+char *getExtention(char *filename) {
+    char *ext = strrchr(filename, '.');
+    if (ext && ext != filename) {
+        return ext + 1;  // returning position after '.'.
+    }
+    return NULL; // No extension found
 }
 
 int searchFile(const char *fpath, const struct stat *sb, int typeflag, struct FTW *ftwbuf) {
@@ -75,7 +86,9 @@ int copyFile (const char *sourceFile, char *filename) {
     strcat(destinationFolder, "/");
     strcat(destinationFolder, filename);
 
+    printf("\nopening source: %s\n", sourceFile);
     FILE *source = fopen(sourceFile, "rb");
+    printf("\nopening dest: %s\n", filename);
     FILE *destination = fopen(destinationFolder, "wb");
 
     // checking if files opened successfully
@@ -115,7 +128,30 @@ int srchCpMv (const char *fpath, const struct stat *sb, int typeflag, struct FTW
     return 0; 
 }
 
+int createTar (const char *fpath, const struct stat *sb, int typeflag, struct FTW *ftwbuf) {
 
+    char *filename = fpath + ftwbuf->base;
+    char *ext = getExtention(filename);
+    printf("%s, %s\n", exten, ext);
+    // comparing extention
+    if (ext != NULL && (strcmp(ext, exten) == 0)) {
+        printf("  %s\n", fpath);
+        printf("Destination: %s\n", destinationFolder);
+        // coping matched file to A1 folder.
+        char cmd[256];
+        snprintf(cmd, sizeof(cmd), "cp %s %s", fpath, destinationFolder);
+      
+        if (system(cmd) == 0) {
+            printf("fileutil: copied successfully");
+        }
+        else {
+            printf("fileutil: error occured while creating an archive");
+        }
+
+        fileCount++; 
+    }
+    return 0; 
+}
 
 int main(int argc, char *argv[]) {
 
@@ -138,6 +174,7 @@ int main(int argc, char *argv[]) {
         if (isSuccess == 0) {
             printf("fileutil: Search Unsuccessful\n");
         }
+        free(targetFile);
         return 0;
     }
     else if (argc == 5) {  // find and move/copy file to given directory.
@@ -159,15 +196,51 @@ int main(int argc, char *argv[]) {
         if (isSuccess == 0) {
             printf("fileutil: Search Unsuccessful\n");
         }
+        free(destinationFolder);
+        free(opr);
+        free(targetFile);
+
         return 0;
-
-
     }
-    else if (argc == 4 ) {  // find and move/copy file with given extention to given dir.
+    else if (argc == 4 ) {  // create tar file of files of given extention. 
         if (validatePath(argv[1])==0 || validatePath(argv[2])==0) {
             printf("fileutil: One or all provided file path is not valid\n");
             return 1;
         }
+
+        //exten = (char *)malloc(128 * sizeof(char));
+        exten = strdup(argv[3]);
+        destinationFolder = argv[2];
+
+        // constructing the destination path.
+        strcat(destinationFolder, "/A1");
+        
+        // create directory to store matching file.
+        int dirStatus = mkdir(destinationFolder, 0777);
+        perror("File status /A1: ");
+
+        int flags = 0;
+        int isSuccess = nftw(argv[1], createTar, 0, flags);
+        if (isSuccess == -1) {
+            printf("fileutil: failure occured\n");
+            return 1;
+        }
+        if (isSuccess == 0) {
+            char cmd[256];
+            snprintf(cmd, sizeof(cmd), "tar -cf %s -C %s .", "A1.tar", destinationFolder);
+
+            if (system(cmd) == 0) {
+                printf("fileutil: tar archieve of %d files created.", fileCount);
+                snprintf(cmd, sizeof(cmd), "rm -r %s", destinationFolder);
+                system(cmd);
+            }
+            else {
+                printf("fileutil: error occured while creating an archive");
+            }
+
+        }
+        return 0;
+
     }
     else if (argc < 3) {
         printf("fileutil: Not enough arguments\n\n");
